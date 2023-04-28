408ce4b1b86eec33c8b82e15d2eb86fd266c5ca9
lmendrick <128844788+lmendrick@users.noreply.github.com>
2023-04-28 14:57:57 -0400

    trying to get bookcase to move when interacted with

--- Assets/Bookcase.cs
+++ Assets/Bookcase.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bookcase : MonoBehaviour
+public class Bookcase : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
 
     private bool hasCrowbar;
     private bool lightIsOn;
+    
+    
     
 
     public string InteractionPrompt => _prompt;
@@ -15,9 +17,10 @@ public class Bookcase : MonoBehaviour
 
     public bool Interact(Interactor interactor)
     {
+        transform.position = Vector3.left * 2;
         if (hasCrowbar && lightIsOn)
         {
-            
+            transform.position = Vector3.left * 2;
         }
         return true;
     }
