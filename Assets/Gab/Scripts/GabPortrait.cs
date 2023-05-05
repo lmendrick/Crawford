7f4d8e883f7de792f775d84b6e62fa9b265c62bc
acampbell34 <acampbell34@fingerlakes.edu>
2023-05-05 15:55:41 -0400

    PORTRAITS!!!

--- Assets/Gab/Scripts/GabPortrait.cs
+++ Assets/Gab/Scripts/GabPortrait.cs
@@ -1,29 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Gab.Scripts;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GabPortrait : MonoBehaviour
 {
-    [SerializeField] private GameObject portrait1;
+    [SerializeField] private Image portrait1;
+    [SerializeField] private Image portrait2;
     [SerializeField] private string name1;
+    [SerializeField] private string name2;
     // Start is called before the first frame update
-    void OnStart()
+    void Start()
     {
         GabManager.OnVariableChanged += ChangePortrait;
     }
 
     void ChangePortrait(string variableName, int value)
     {
-        if (variableName == name1) {
+        Debug.Log("VARIABLECHAGNE " + variableName + " " + value);
+
+        if (variableName == name1)
+        {
+            Debug.Log("Variable Name is name 1 " + variableName);
+
             if (value == 1)
-                portrait1.SetActive(true);
-            else
-                portrait1.SetActive(false);
+                portrait1.color = new Color(1, 1, 1, 1);
+
+            if (value == 0)
+                portrait1.color = new Color(1, 1, 1, 0);
+        }
+
+        if (variableName == name2)
+
+        {
+
+            if (value == 1)
+                portrait2.color = new Color(1, 1, 1, 1);
+
+            if (value == 0)
+                portrait2.color = new Color(1, 1, 1, 0);
+
         }
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
